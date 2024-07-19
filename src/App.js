a06a548e24b84877a66c70d1dabc1d65255b37f9
Vibhav Kulshrestha <vibhav.kul@gmail.com>
2024-07-19 12:55:15 +0530

    Header Change

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <div className="App">
         <header className="App-header">
           <h1>Categories and Products Management</h1>
+          <h2>Vibhav's Distributers</h2>
         </header>
         <main>
           <Routes>
